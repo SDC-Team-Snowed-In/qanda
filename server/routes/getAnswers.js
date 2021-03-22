98936eb3a84bc7cf1cc5110522294772a453321e
jbframe <framejb@gmail.com>
2021-03-21 21:33:54 -0600

    Updated git ignore

--- server/routes/getAnswers.js
+++ server/routes/getAnswers.js
@@ -54,3 +54,9 @@ router.get('/:question_id/answers', async (req, res) => {
 
   res.send(result)
 })
+
+
+[{"id":1,
+ "url":"https://images.unsplash.com/photo-1530519729491-aea5b51d1ee1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80"}, {"id":2,
+ "url":"https://images.unsplash.com/photo-1511127088257-53ccfcc769fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"}, {"id":3,
+ "url":"https://images.unsplash.com/photo-1500603720222-eb7a1f997356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"}]
